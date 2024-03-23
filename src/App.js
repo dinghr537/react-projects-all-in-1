bca56a6de52aac1a05b7362212073999efcb3e24
Haoran Ding <hda58@sfu.ca>
2024-03-22 17:59:44 -0700

    复杂条件渲染（用函数）

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 
-const isLogin = false
+const articalType = 3 // 0 1 3: 无图，单图，三图
 
-function getName() {
-  return 'iris'
+
+function getArticaltemplate() {
+  if (articalType == 0) {
+    return <div>无图文章</div>
+  } else if (articalType == 1) {
+    return <div>单图文章</div>
+  } else {
+    return <div>三图文章</div> 
+  }
 }
 
 function App() {
   return (
     <div className="App">
       {/* this is app */}
-      {isLogin && <span>this is span</span>}
-      {isLogin ? <span>this is span</span> : <span>loading...</span>}
+      {getArticaltemplate()}
     </div>
   );
 }
