434866778412c26d92568cfe1e646af4a30b89c5
Haoran Ding <hda58@sfu.ca>
2024-03-22 18:10:48 -0700

    事件绑定

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 
-const articalType = 3 // 0 1 3: 无图，单图，三图
 
-
-function getArticaltemplate() {
-  if (articalType == 0) {
-    return <div>无图文章</div>
-  } else if (articalType == 1) {
-    return <div>单图文章</div>
-  } else {
-    return <div>三图文章</div> 
-  }
-}
 
 function App() {
+  // const handleClick = () => {
+  //   console.log("CLICKKKKKKK")
+  // }
+  const handleClick = (e) => {
+    console.log("CLICKKKKKKK", e)
+  }
+  const selfDefinedClickhandler = (name) => {
+    console.log(name)
+  }
+  const selfDefinedAndE = (name, e) => {
+    console.log(name, e)
+  }
   return (
     <div className="App">
-      {/* this is app */}
-      {getArticaltemplate()}
+      <button onClick={handleClick}>click me</button>
+      <button onClick={() => selfDefinedClickhandler('iris')}>and me</button>
+      <button onClick={(e) => selfDefinedAndE('iris', e)}>and me</button>
     </div>
   );
 }
