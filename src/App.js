9a3c94844eee16da277ed93619ae082ab11d6df6
Haoran Ding <hda58@sfu.ca>
2024-03-22 18:18:29 -0700

    基础组件使用

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 
-
+function Button() {
+  return <button>click me</button>
+}
 
 function App() {
   // const handleClick = () => {
   //   console.log("CLICKKKKKKK")
   // }
-  const handleClick = (e) => {
-    console.log("CLICKKKKKKK", e)
-  }
-  const selfDefinedClickhandler = (name) => {
-    console.log(name)
-  }
-  const selfDefinedAndE = (name, e) => {
-    console.log(name, e)
-  }
+
   return (
     <div className="App">
-      <button onClick={handleClick}>click me</button>
-      <button onClick={() => selfDefinedClickhandler('iris')}>and me</button>
-      <button onClick={(e) => selfDefinedAndE('iris', e)}>and me</button>
+      <Button />
     </div>
   );
 }
