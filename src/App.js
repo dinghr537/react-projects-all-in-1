596416b14a709b46c75e16053593402102e6d52e
Haoran Ding <hda58@sfu.ca>
2024-03-25 17:48:32 -0700

    通过接口获取数据 useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.scss'
 import avatar from './images/bozai.png'
 import _ from 'lodash'
 import classNames from 'classnames'
 import {v4 as uuidV4} from 'uuid'
 import dayjs from 'dayjs'
+import axios from 'axios'
 
 /**
  * 评论列表的渲染和操作
@@ -14,45 +15,45 @@ import dayjs from 'dayjs'
  */
 
 // 评论列表数据
-const list = [
-  {
-    // 评论id
-    rpid: 3,
-    // 用户信息
-    user: {
-      uid: '13258165',
-      avatar: '',
-      uname: '周杰伦',
-    },
-    // 评论内容
-    content: '哎哟，不错哦',
-    // 评论时间
-    ctime: '10-18 08:15',
-    like: 88,
-  },
-  {
-    rpid: 2,
-    user: {
-      uid: '36080105',
-      avatar: '',
-      uname: '许嵩',
-    },
-    content: '我寻你千百度 日出到迟暮',
-    ctime: '11-13 11:29',
-    like: 88,
-  },
-  {
-    rpid: 1,
-    user: {
-      uid: '30009257',
-      avatar,
-      uname: '黑马前端',
-    },
-    content: '学前端就来黑马',
-    ctime: '10-19 09:00',
-    like: 66,
-  },
-]
+// const list = [
+//   {
+//     // 评论id
+//     rpid: 3,
+//     // 用户信息
+//     user: {
+//       uid: '13258165',
+//       avatar: '',
+//       uname: '周杰伦',
+//     },
+//     // 评论内容
+//     content: '哎哟，不错哦',
+//     // 评论时间
+//     ctime: '10-18 08:15',
+//     like: 88,
+//   },
+//   {
+//     rpid: 2,
+//     user: {
+//       uid: '36080105',
+//       avatar: '',
+//       uname: '许嵩',
+//     },
+//     content: '我寻你千百度 日出到迟暮',
+//     ctime: '11-13 11:29',
+//     like: 88,
+//   },
+//   {
+//     rpid: 1,
+//     user: {
+//       uid: '30009257',
+//       avatar,
+//       uname: '黑马前端',
+//     },
+//     content: '学前端就来黑马',
+//     ctime: '10-19 09:00',
+//     like: 66,
+//   },
+// ]
 // 当前登录用户信息
 const user = {
   // 用户id
@@ -79,10 +80,18 @@ const tabs = [
 ]
 
 const App = () => {
-  const [commentList, setCommentList] = useState(_.orderBy(list, 'like', 'desc'))
+  const [commentList, setCommentList] = useState([])
   const [type, setType] = useState('hot')
   const [content, setContent] = useState('')
   const inputRef = useRef(null)
+
+  useEffect(()=>{
+    async function getList() {
+      const res = await axios.get('http://localhost:3004/list')
+      setCommentList(res.data)
+    }
+    getList()
+  }, [])
 
   const handleDelete = (id) => {
     // console.log(id)
