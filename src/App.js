4bf3b9353daa393bef97cae42be4a8beb2987427
Haoran Ding <hda58@sfu.ca>
2024-03-25 17:55:10 -0700

    自定义hook封装获取数据

--- src/App.js
+++ src/App.js
@@ -7,53 +7,6 @@ import {v4 as uuidV4} from 'uuid'
 import dayjs from 'dayjs'
 import axios from 'axios'
 
-/**
- * 评论列表的渲染和操作
- *
- * 1. 根据状态渲染评论列表
- * 2. 删除评论
- */
-
-// 评论列表数据
-// const list = [
-//   {
-//     // 评论id
-//     rpid: 3,
-//     // 用户信息
-//     user: {
-//       uid: '13258165',
-//       avatar: '',
-//       uname: '周杰伦',
-//     },
-//     // 评论内容
-//     content: '哎哟，不错哦',
-//     // 评论时间
-//     ctime: '10-18 08:15',
-//     like: 88,
-//   },
-//   {
-//     rpid: 2,
-//     user: {
-//       uid: '36080105',
-//       avatar: '',
-//       uname: '许嵩',
-//     },
-//     content: '我寻你千百度 日出到迟暮',
-//     ctime: '11-13 11:29',
-//     like: 88,
-//   },
-//   {
-//     rpid: 1,
-//     user: {
-//       uid: '30009257',
-//       avatar,
-//       uname: '黑马前端',
-//     },
-//     content: '学前端就来黑马',
-//     ctime: '10-19 09:00',
-//     like: 66,
-//   },
-// ]
 // 当前登录用户信息
 const user = {
   // 用户id
@@ -64,27 +17,14 @@ const user = {
   uname: '黑马前端',
 }
 
-/**
- * 导航 Tab 的渲染和操作
- *
- * 1. 渲染导航 Tab 和高亮
- * 2. 评论列表排序
- *  最热 => 喜欢数量降序
- *  最新 => 创建时间降序
- */
-
 // 导航 Tab 数组
 const tabs = [
   { type: 'hot', text: '最热' },
   { type: 'time', text: '最新' },
 ]
 
-const App = () => {
+function useGetList() {
   const [commentList, setCommentList] = useState([])
-  const [type, setType] = useState('hot')
-  const [content, setContent] = useState('')
-  const inputRef = useRef(null)
-
   useEffect(()=>{
     async function getList() {
       const res = await axios.get('http://localhost:3004/list')
@@ -92,6 +32,18 @@ const App = () => {
     }
     getList()
   }, [])
+  return {
+    commentList,
+    setCommentList
+  }
+}
+
+const App = () => {
+  
+  const [type, setType] = useState('hot')
+  const [content, setContent] = useState('')
+  const inputRef = useRef(null)
+  const {commentList, setCommentList} = useGetList()
 
   const handleDelete = (id) => {
     // console.log(id)
