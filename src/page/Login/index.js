72e4a07c400a2d7b88b0a046618d94d81c43737b
Haoran Ding <dinghr537@gmail.com>
2024-03-31 17:53:19 -0700

    路由导航跳转

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,6 +1,21 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+
+// option 1: <Link>
+
+// option 2: useNavigate
 const Login = () => {
+    const navigate = useNavigate()
+
     return (
-        <div>LOGIN ...</div>
+        <>
+            <div>LOGIN ...</div>
+
+            <Link to="/article">go to article page</Link>
+
+            <button onClick={()=>navigate('/article')}>go</button>
+        </>
+        
     )
 }
 
