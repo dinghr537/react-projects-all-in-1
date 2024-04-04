995508a2222a5966feedfdb9dc99b578dc02cde7
Haoran Ding <dinghr537@gmail.com>
2024-04-03 22:01:35 -0700

    数据按月分组

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,10 +1,17 @@
 import { NavBar, DatePicker } from 'antd-mobile'
 import './index.scss'
-import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useMemo, useState } from 'react'
 import classNames from 'classnames'
 import dayjs from 'dayjs'
+import _ from 'lodash'
 
 const Month = () => {
+  const billList = useSelector(state => state.bill.billList)
+  const monthGroup = useMemo(() => {
+    return _.groupBy(billList, (item)=>dayjs(item.date).format('YYYY-MM'))
+  }, [billList])
+  console.log(monthGroup)
   const [dateVisible, setDateVisible] = useState(false)
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().format('YYYY-MM')
